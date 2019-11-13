42eaa35e11d8494833b1439667d53fd05828d5a6
BreadyX <alex.bradatan85@yahoo.it>
2019-11-13 14:06:10 +0100

    Eseguito ES5 degli esercizi di laboratorio del 12 novembre

--- fondamentiinfo/lab/121119.c
+++ fondamentiinfo/lab/121119.c
@@ -3,18 +3,33 @@
 #include <time.h>
 
 /* #define MAT_DIM 2 Per es 2*/
-#define MAT_DIM 3
-#define STR_LEN 50
+#define MAT_DIM   3
+#define DIM       10
+#define SOGLIA    10
+#define STR_LEN   50
 
 typedef float t_matrice[MAT_DIM][MAT_DIM];
+typedef struct {
+    int v_pari[DIM];
+    int n_pari;
+    int v_dispari[DIM];
+    int n_dispari;
+} s_risultato;
 
 float conversione(float in);
+
 int genera_matrice(int mat[MAT_DIM][MAT_DIM], int *sum);
+
 void getindex(char str[STR_LEN + 1], int from, char delim, int *index);
 void estrazione(char result[STR_LEN + 1], char orig[STR_LEN + 1], char delim);
+
 void calcola(t_matrice matr_media, int matr_orig[MAT_DIM][MAT_DIM]);
 float media(int matr_orig[MAT_DIM][MAT_DIM], int i, int j);
 int dentro(int x, int y);
+
+void stampavet(const int vet[DIM], int dim);
+void inserisci(int vet[DIM], int pos, int val);
+s_risultato analisi(int vet[DIM], int *sotto_s);
 
 int main(void)
 {
@@ -48,27 +63,44 @@ int main(void)
     /* printf("Orig: \"%s\" | Splice: \"%s\" | Delim: \"%c\"\n", str, splice, delim); */
 
     // ### ES4 ###
-    int mat[MAT_DIM][MAT_DIM], i, j;
-    t_matrice media;
+    /* int mat[MAT_DIM][MAT_DIM], i, j; */
+    /* t_matrice media; */
+    /* srand(time(NULL)); */
+    /* for (i = 0; i < MAT_DIM; i++) { */
+    /*     for (j = 0; j < MAT_DIM; j++) { */
+    /*         mat[i][j] = rand() % 100; */
+    /*         printf("%d ", mat[i][j]); */
+    /*     } */
+    /*     printf("\n"); */
+    /* } */
+    /* printf("\n"); */
+    /* calcola(media, mat); */
+    /* for (i = 0; i < MAT_DIM; i++) { */
+    /*     for (j = 0; j < MAT_DIM; j++) */
+    /*         printf("%.2f ", media[i][j]); */
+    /*     printf("\n"); */
+    /* } */
+
+    // ### ES5 ###
+    int vet[DIM], i, sotto_s;
+    s_risultato ris;
+
     srand(time(NULL));
-    for (i = 0; i < MAT_DIM; i++) {
-        for (j = 0; j < MAT_DIM; j++) {
-            mat[i][j] = rand() % 100;
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    calcola(media, mat);
-    for (i = 0; i < MAT_DIM; i++) {
-        for (j = 0; j < MAT_DIM; j++)
-            printf("%.2f ", media[i][j]);
-        printf("\n");
-    }
+    for (i = 0; i < DIM; i++)
+        vet[i] = rand() % 100;
+    printf("Vettore orig: ");
+    stampavet(vet, DIM);
+    ris = analisi(vet, &sotto_s);
+    printf("Vettore pari: ");
+    stampavet(ris.v_pari, ris.n_pari);
+    printf("Vettore dispari: ");
+    stampavet(ris.v_dispari, ris.n_dispari);
+
     return 0;
 }
 
 float conversione(float in) { return ((in - 32) * 5.0 / 9.0); }
+
 int genera_matrice(int mat[MAT_DIM][MAT_DIM], int *sum)
 {
     int i, j;
@@ -92,6 +124,7 @@ void getindex(char str[STR_LEN + 1], int from, char delim, int *index)
         }
     }
 }
+
 void estrazione(char result[STR_LEN + 1], char orig[STR_LEN + 1], char delim)
 {
     int start = -1, end = -1, i;
@@ -109,6 +142,7 @@ void estrazione(char result[STR_LEN + 1], char orig[STR_LEN + 1], char delim)
         result[i - start - 1] = orig[i];
     result[i - start - 1] = '\0';
 }
+
 void calcola(t_matrice mat_media, int mat_orig[MAT_DIM][MAT_DIM])
 {
     int i, j;
@@ -132,4 +166,34 @@ float media(int mat_orig[MAT_DIM][MAT_DIM], int i, int j)
     return ((float)sum) / n;
 }
 int dentro(int i, int j)
-{ return (i >= 0 && i  < MAT_DIM) && (j >= 0 && j < MAT_DIM); }
+{ return (i >= 0 && i < MAT_DIM) && (j >= 0 && j < MAT_DIM); }
+
+void stampavet(const int vet[DIM], int n)
+{
+    for(int i = 0; i < n; i++)
+        printf("%d ", vet[i]);
+    printf("\n");
+}
+void inserisci(int vet[DIM], int pos, int val) { vet[pos] = val; }
+s_risultato analisi(int vet[DIM], int *sotto_s)
+{
+    int i, i_p, i_d;
+    s_risultato ris;
+    ris.n_pari = 0;
+    ris.n_dispari = 0;
+    for (i = 0, i_p = 0, i_d = 0; i < DIM; i++) {
+        if (vet[i] < SOGLIA) {
+            (*sotto_s)++;
+            if (vet[i] % 2 == 0) {
+                inserisci(ris.v_pari, i_p, vet[i]);
+                i_p++;
+                ris.n_pari++;
+            } else {
+                inserisci(ris.v_dispari, i_d, vet[i]);
+                i_d++;
+                ris.n_dispari++;
+            }
+        }
+    }
+    return ris;
+}
