e57b17eafa0e0f749ab77caded6e1b7bfde3a715
BreadyX <alex.bradatan85@yahoo.it>
2021-02-25 19:01:54 +0100

    Aggiunto opzione `--toc-depth` a `md_to_pdf.sh`

--- 0-utils/md_to_pdf.sh
+++ 0-utils/md_to_pdf.sh
@@ -3,11 +3,19 @@
 [ -x /usr/bin/pandoc ] || { echo "pandoc is required"; exit 1; }
 [ -x /usr/bin/realpath ] || { echo "realpath is required"; exit 1; }
 
-TOC="--toc --toc-depth=4"
+TOC="--toc"
 case "$1" in
 	""|"--help"|"-h")
-		echo "md_to_pdf.sh [ --no-toc ] input output"
+		echo "md_to_pdf.sh [ --no-toc | --toc-depth N ] input output"
 		exit 0 ;;
+	"--toc-depth")
+		if [ -z "$(echo "$2"| grep -Ev '[0-9]+')" ]; then
+			TOC+=" --toc-depth=$2"
+			shift 2
+		else
+			echo "Invalid number: '$2'"
+			exit 1
+		fi ;;
 	"--no-toc")
 		TOC=""
 		shift 1 ;;
