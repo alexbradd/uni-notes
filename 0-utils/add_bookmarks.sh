6e00b7c47f0e8dae1780580f4415d0cd4be982e6
BreadyX <alex.bradatan85@yahoo.it>
2021-03-03 22:54:46 +0100

    add_bookmarks: modificato ordine parametri

--- 0-utils/add_bookmarks.sh
+++ 0-utils/add_bookmarks.sh
@@ -4,12 +4,24 @@
 [ -x /usr/bin/realpath ] || { echo "realpath is required"; exit 1; }
 
 if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = '-h' ]; then
-	echo "add_bookmarks.sh input bookmarks"
+	echo "add_bookmarks.sh input bookmarks output"
 	exit 0
 fi
 
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+	echo 'Missing path'
+	exit 1
+fi
+
 INPUT="$(realpath "$1")"
+OUTPUT="$(realpath "$3")"
 BOOKMARKS="$(realpath "$2")"
 
-gs -o /tmp/book_tmp.pdf -sDEVICE=pdfwrite "$BOOKMARKS" -f "$INPUT" &&
-	mv /tmp/book_tmp.pdf "$INPUT"
+if [ "$INPUT" = "$OUTPUT" ]; then
+	echo 'Output cannot be the same as input'
+	exit 1
+fi
+
+gs -dBATCH -dNOPAUSE -dSAFER -sDEVICE=pdfwrite -sOutputFile="$OUTPUT" \
+	"$BOOKMARKS" -f "$INPUT"
+
