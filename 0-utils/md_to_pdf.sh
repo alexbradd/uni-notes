b3a1d2572a0a68955f925e92f422ced92e0fd3ce
Alexandru Gabriel Bradatan <alex.bradatan85@yahoo.it>
2021-09-25 18:34:21 +0200

    Aggiungi filtro per plantuml a md_to_pdf.sh

--- 0-utils/md_to_pdf.sh
+++ 0-utils/md_to_pdf.sh
@@ -4,9 +4,10 @@ which pandoc >/dev/null 2>&1   || { echo "pandoc is required"; exit 1; }
 which realpath >/dev/null 2>&1 || { echo "realpath is required"; exit 1; }
 
 TOC="--toc"
+ADDITIONAL_OPTS=""
 case "$1" in
 	""|"--help"|"-h")
-		echo "md_to_pdf.sh [ --no-toc | --toc-depth N ] input output"
+		echo "md_to_pdf.sh [ --no-toc | --toc-depth N | --plantuml ] input output"
 		exit 0 ;;
 	"--toc-depth")
 		if [ -z "$(echo "$2"| grep -Ev '[0-9]+')" ]; then
@@ -19,6 +20,17 @@ case "$1" in
 	"--no-toc")
 		TOC=""
 		shift 1 ;;
+	"--plantuml")
+		which plantuml >/dev/null 2>&1 || {
+			echo "plantuml is required"
+			exit 1
+		}
+		which pandoc-plantuml >/dev/null 2>&1 || {
+			echo "pandoc-plantuml is required"
+			exit 1
+		}
+		ADDITIONAL_OPTS+="--filter pandoc-plantuml"
+		shift ;;
 esac
 
 INPUT="$(realpath "$1")"
@@ -28,6 +40,7 @@ pandoc "$INPUT" \
 	-o "$OUTPUT" \
 	-t pdf \
 	$TOC \
+	$ADDITIONAL_OPTS \
 	--shift-heading-level-by=-1 \
 	--pdf-engine="pdflatex" \
 	-V "documentclass:article" \
